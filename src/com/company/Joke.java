488bbcb9e4f478153b149176476fa5a49b9d0cf4
Monika <karczewska.mon@gmail.com>
2022-02-22 19:40:25 +0100

    joke constructor created

--- src/com/company/Joke.java
+++ src/com/company/Joke.java
@@ -1,6 +1,10 @@
 package com.company;
 
-public class Joke {
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+public class Joke implements Serializable {
     public String category;
     public String type;
     public String joke;
@@ -30,6 +34,24 @@ public class Joke {
         } else {
             return this.setup + " " + delivery;
         }
+    }
+
+    public Joke (JSONObject jokeObject){
+        category = jokeObject.getString("category");
+        type = jokeObject.getString("type");
+        joke = jokeObject.has("joke") ? jokeObject.getString("joke") : null;
+        setup = jokeObject.has("setup") ? jokeObject.getString("setup") : null;
+        delivery = jokeObject.has("delivery") ? jokeObject.getString("delivery") : null;
+        // this is the same as: if(jokeObject.has("delivery")){joke.delivery=jokeObject.getString("delivery");} else {joke.delivery = null;}
+        id = jokeObject.getInt("id");
+        safe = jokeObject.getBoolean("safe");
+        language = jokeObject.getString("lang");
+        nsfw = jokeObject.getJSONObject("flags").getBoolean("nsfw");
+        religious = jokeObject.getJSONObject("flags").getBoolean("religious");
+        political = jokeObject.getJSONObject("flags").getBoolean("political");
+        racist = jokeObject.getJSONObject("flags").getBoolean("racist");
+        sexist = jokeObject.getJSONObject("flags").getBoolean("sexist");
+        explicit = jokeObject.getJSONObject("flags").getBoolean("explicit");
     }
 
 }
--- src/com/company/JokeConnector.java
+++ src/com/company/JokeConnector.java
@@ -59,22 +59,7 @@ public class JokeConnector {
         JSONTokener tokener = new JSONTokener(jokeData);
         JSONObject jokeObject = new JSONObject(tokener);
 
-        Joke joke = new Joke();
-        joke.category = jokeObject.getString("category");
-        joke.type = jokeObject.getString("type");
-        joke.joke = jokeObject.has("joke") ? jokeObject.getString("joke") : null;
-        joke.setup = jokeObject.has("setup") ? jokeObject.getString("setup") : null;
-        joke.delivery = jokeObject.has("delivery") ? jokeObject.getString("delivery") : null;
-        // this is the same as: if(jokeObject.has("delivery")){joke.delivery=jokeObject.getString("delivery");} else {joke.delivery = null;}
-        joke.id = jokeObject.getInt("id");
-        joke.safe = jokeObject.getBoolean("safe");
-        joke.language = jokeObject.getString("lang");
-        joke.nsfw = jokeObject.getJSONObject("flags").getBoolean("nsfw");
-        joke.religious = jokeObject.getJSONObject("flags").getBoolean("religious");
-        joke.political = jokeObject.getJSONObject("flags").getBoolean("political");
-        joke.racist = jokeObject.getJSONObject("flags").getBoolean("racist");
-        joke.sexist = jokeObject.getJSONObject("flags").getBoolean("sexist");
-        joke.explicit = jokeObject.getJSONObject("flags").getBoolean("explicit");
+        Joke joke = new Joke(jokeObject);
 
         return joke;
 
